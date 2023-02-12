5a3ab5c13537d993b336282932220bfb66e8e5d0
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2023-02-12 12:28:18 -0600

    models folder with user, thought, and reaction models

---
+++ models/reaction.js
@@ -0,0 +1,33 @@
+const {Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 200
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: timestamp => dateFormat(timestamp)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+)
+
+modules.export = 'reactionSchema';
